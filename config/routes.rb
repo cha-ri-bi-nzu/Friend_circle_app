812636f0cb0dbc43e2815ede10aab0b823afe1d2
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-06-15 22:56:29 +0900

    ルートの編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,6 @@
 Rails.application.routes.draw do
-  get 'pictures/index'
-  get 'pictures/new'
-  get 'pictures/create'
-  get 'pictures/confirm'
-  get 'pictures/show'
-  get 'pictures/edit'
-  get 'pictures/update'
-  get 'pictures/destroy'
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  get 'users/new'
-  get 'users/create'
-  get 'users/show'
+  resources :pictures 
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
