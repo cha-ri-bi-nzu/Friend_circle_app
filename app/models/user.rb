5bbf96cdb304663b9d53660d93f54b10fc449038
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-06-15 23:16:52 +0900

    バリデーション機能修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   before_validation { email.downcase! }
-  validates :name, presence: true
+  validates :name, presence: {message: "空白以外の文字を入力して下さい(>_<)"}, length: { maximum: 30 }
+  validates :email, presence: {message: "空白以外の文字を入力して下さい(>_<)"}, length: { maximum: 255}
   validates :password, length: { minimum: 6 }
     
   has_secure_password
