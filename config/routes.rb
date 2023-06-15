8117f30a1256ee224b97df172f7fa67132760eba
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-06-15 21:49:28 +0900

    コントローラーとビュー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'pictures/index'
+  get 'pictures/new'
+  get 'pictures/create'
+  get 'pictures/confirm'
+  get 'pictures/show'
+  get 'pictures/edit'
+  get 'pictures/update'
+  get 'pictures/destroy'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  get 'users/new'
+  get 'users/create'
+  get 'users/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
