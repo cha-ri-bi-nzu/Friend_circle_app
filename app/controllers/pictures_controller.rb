82b01b95ad6358e8e17a40809e7b7efefa08c72f
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-06-18 18:04:22 +0900

    パーシャル作成

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,11 +1,20 @@
 class PicturesController < ApplicationController
   def index
+    @pictures = Picture.all
   end
 
   def new
+    @picture = Picture.new
   end
 
   def create
+    @picture = Picture.new(picture_params)
+    @picture = current_user.pictures.build(picture_params)
+    if @picture.save
+      redirect_to pictures_path, notice: "投稿でけた👍"
+    else
+      render :new
+    end
   end
 
   def confirm
@@ -21,5 +30,15 @@ class PicturesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:image, :image_cache)
+  end
+
+  def set_picture
+    
   end
 end
